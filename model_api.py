dc47b3caebafaf0c797ce6eb7daadcadac71246d
mf.rohman0101 <mf.rohman0101@gmail.com>
2025-06-08 19:22:46 +0000

    fix hitory analisis-3

--- model_api.py
+++ model_api.py
@@ -131,7 +131,7 @@ def predict():
             "email": user_email,
             "name": user_name,
             "imageUrl": image_url,
-            "analysisResult": jsonif(yresult),
+            "analysisResult": jsonify(result),
             "recommendation": result["nutrition_status"]
         }
 
